dfb0c34b80aa8142007f7361d665aebb6145d38a
Brian Manning <brian@xaoc.org>
2013-06-23 16:28:29 -0700

    glib_test-options: run 'make test' instead of prove
    - Need to figure out how 'make test' sets up the environment, so you
      can run 'prove' the same way in order to just run 'options.t'

--- glib_test-options.t.sh
+++ glib_test-options.t.sh
@@ -3,10 +3,10 @@ if [ ! -e "t/options.t" ]; then
     echo "Hey! Need to run this script in the Glib source tree"
     exit 1
 fi
-LANG=de_DE.ISO8859-1 LC_ALL= prove -v t/options.t
-LANG=de_DE.ISO8859-1 LC_ALL=hr_HR.ISO8859-2 prove -v t/options.t
-LANG=de_DE.ISO8859-1 LC_ALL=C prove -v t/options.t
-LANG=de_DE.ISO8859-1 LC_ALL=POSIX prove -v t/options.t
-LANG= LC_ALL= prove -v t/options.t
-LANG= LC_ALL=de_DE.UTF-8 prove -v t/options.t
-LANG= LC_ALL=en_GB.US-ASCII prove -v t/options.t
+LANG=de_DE.ISO8859-1 LC_ALL= make test
+LANG=de_DE.ISO8859-1 LC_ALL=hr_HR.ISO8859-2 make test
+LANG=de_DE.ISO8859-1 LC_ALL=C make test
+LANG=de_DE.ISO8859-1 LC_ALL=POSIX make test
+LANG= LC_ALL= make test
+LANG= LC_ALL=de_DE.UTF-8 make test
+LANG= LC_ALL=en_GB.US-ASCII make test
